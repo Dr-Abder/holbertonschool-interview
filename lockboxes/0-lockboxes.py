5833080c770ece0946b10b88072981c9edb7bb8e
Dr-Abder <9907@holbertonstudents.com>
2025-08-29 13:48:47 +0000

    task 0-lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Module 0-lockboxes
+Détermine si toutes les boîtes peuvent être ouvertes à partir de la boîte 0.
+"""
+
 
 def canUnlockAll(boxes):
+
+    """
+    Détermine si toutes les boîtes peuvent être ouvertes.
+    
+    boxes: list de listes, chaque liste contient les clés présentes dans la boîte.
+    Retourne True si toutes les boîtes peuvent être ouvertes, sinon False.
+    """
 
     visited = {0}   # ensemble contenant juste la boîte 0
     stack = [0]     # pile pour explorer les boîtes
