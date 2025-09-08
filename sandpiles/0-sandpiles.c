125cdae65b2e940b1c29d92db8fbe8bf34af07f1
Dr-Abder <9907@holbertonstudents.com>
2025-09-08 11:21:07 +0000

    task 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -2,97 +2,121 @@
 #include <stdio.h>
 #include "sandpiles.h"
 
+
+/**
+* sandpiles_sum - Print la somme de deux tableaux avec des conditions
+* @grid1: 3x3 grid
+* @grid2: 3x3 grid
+*
+*/
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, x;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (x = 0; x < 3; x++)
-        {
-            grid1[i][x] += grid2[i][x];
-        }
-    }
-    while (is_stable(grid1) == 0)
-    {
-        printf("=\n");
-        print_grid(grid1);
-        topple(grid1);
-    }
+int i, x;
+for (i = 0; i < 3; i++)
+{
+for (x = 0; x < 3; x++)
+{
+grid1[i][x] += grid2[i][x];
+}
+}
+/**
+* topple - faire un topple si grid1 n'est pas stable
+*/
+while (!is_stable(grid1))
+{
+printf("=\n");
+print_grid(grid1);
+topple(grid1);
+}
 }
 
+/**
+ * print_grid - Prints a 3x3 grid
+ * @grid: 3x3 grid à print
+ */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j) 
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+int i, j;
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
+}
 }
 
+/**
+ * is_stable - Vérifier si grid est stable
+ * stable = grid[i][x] < 3
+ * Return: si c'est stable 1 sinon 0
+ * @grid: 3x3 grid à vérifier
+ */
 int is_stable(int grid[3][3])
 {
-    int i, x;
+int i, x;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (x = 0; x < 3; x++)
-        {
-            if (grid[i][x] > 3)
-            {
-                return 0;
-            }
-        }
-    }
-    return 1;
+for (i = 0; i < 3; i++)
+{
+for (x = 0; x < 3; x++)
+{
+if (grid[i][x] > 3)
+{
+return (0);
+}
+}
+}
+return (1);
 }
 
+/**
+* topple - faire un topple si grid1 n'est pas stable
+* si n'est pas stable faire -4 et attribuer +1 à ces voisins direct
+* @grid: 3x3 grid à stabiliser
+*/
 void topple(int grid[3][3])
 {
-    int temp[3][3] = {0};
-    int i, x;
+int temp[3][3] = {0};
+int i, x;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (x =0; x < 3; x++)
-        {
-            if (grid[i][x] >= 4)
-            {
-                grid[i][x] -= 4;
+for (i = 0; i < 3; i++)
+{
+for (x = 0; x < 3; x++)
+{
+if (grid[i][x] >= 4)
+{
+grid[i][x] -= 4;
 
-                if (i > 0)
-                {
-                    temp[i-1][x] += 1;
-                }
+if (i > 0)
+{
+temp[i - 1][x] += 1;
+}
 
-                if (i < 2)
-                {
-                    temp[i+1][x] += 1;
-                }
+if (i < 2)
+{
+temp[i + 1][x] += 1;
+}
 
-                if (x > 0)
-                {
-                    temp[i][x-1] += 1;
-                }
+if (x > 0)
+{
+temp[i][x - 1] += 1;
+}
 
-                if (x < 2)
-                {
-                    temp[i][x+1] += 1;
-                }
-            }
-        }
-    }
-    for (i = 0; i < 3; i++)
-    {
-        for (x = 0; x < 3; x++)
-        {
-            grid[i][x] += temp[i][x];
-        }
-    }
+if (x < 2)
+{
+temp[i][x + 1] += 1;
+}
+}
+}
+}
+for (i = 0; i < 3; i++)
+{
+for (x = 0; x < 3; x++)
+{
+grid[i][x] += temp[i][x];
+}
+}
 }
--- sandpiles/1-main.c
+++ sandpiles/1-main.c
@@ -1,84 +1,84 @@
-#include <stdlib.h>
-#include <stdio.h>
+    #include <stdlib.h>
+    #include <stdio.h>
 
-#include "sandpiles.h"
+    #include "sandpiles.h"
 
-/**
- * print_grid_sum - Print 3x3 grids sum
- * @grid1: Left 3x3 grid
- * @grid2: Right 3x3 grid
- *
- */
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
+    /**
+    * print_grid_sum - Print la somme de deux tableaux
+    * @grid1: 3x3 grid
+    * @grid2: 3x3 grid
+    *
+    */
+    static void print_grid_sum(int grid1[3][3], int grid2[3][3])
     {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
-        }
+        int i, j;
 
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
+        for (i = 0; i < 3; i++)
         {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
+            for (j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid1[i][j]);
+            }
+
+            printf(" %c ", (i == 1 ? '+' : ' '));
+
+            for (j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid2[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-}
 
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
- */
-static void print_grid(int grid[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
+    /**
+    * print_grid - Print 3x3 grid
+    * @grid: 3x3 grid
+    *
+    */
+    static void print_grid(int grid[3][3])
     {
-        for (j = 0; j < 3; j++)
+        int i, j;
+
+        for (i = 0; i < 3; i++)
         {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
+            for (j = 0; j < 3; j++)
+            {
+                if (j)
+                    printf(" ");
+                printf("%d", grid[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
     }
-}
 
-/**
- * main - Entry point
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
- */
-int main(void)
-{
-    int grid1[3][3] = {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    };
-    int grid2[3][3] = {
-        {3, 3, 3},
-        {3, 3, 3},
-        {3, 3, 3}
-    };
+    /**
+    * main - Entry point
+    *
+    * Return: EXIT_SUCCESS or EXIT_FAILURE
+    */
+    int main(void)
+    {
+        int grid1[3][3] = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        };
+        int grid2[3][3] = {
+            {3, 3, 3},
+            {3, 3, 3},
+            {3, 3, 3}
+        };
 
-    print_grid_sum(grid1, grid2);
+        print_grid_sum(grid1, grid2);
 
-    sandpiles_sum(grid1, grid2);
+        sandpiles_sum(grid1, grid2);
 
-    printf("=\n");
-    print_grid(grid1);
+        printf("=\n");
+        print_grid(grid1);
 
-    return (EXIT_SUCCESS);
-}
+        return (EXIT_SUCCESS);
+    }
