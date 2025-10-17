2efc916152ec9462a189ac3c933c9af34ffe9785
Dr-Abder <9907@holbertonstudents.com>
2025-10-17 16:40:10 +0000

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -4,31 +4,31 @@ UTF-8 encodage
 """
 
 def validUTF8(data):
-    """
-    Détermine si data représente un encodage UTF-8 valide
-    """
+"""
+Détermine si data représente un encodage UTF-8 valide
+"""
+n_byte = 0
+
+for num in data:
+
+byte = num & 0xFF
+
+if n_byte == 0:
+if (byte & 0b10000000) == 0b00000000:
     n_byte = 0
+elif (byte & 0b11100000) == 0b11000000:
+    n_byte = 1
+elif (byte & 0b11110000) == 0b11100000:
+    n_byte = 2
+elif (byte & 0b11111000) == 0b11110000:
+    n_byte = 3
+else:
+    return False
 
-    for num in data:
+elif n_byte > 0:
+if (byte & 0b11000000) == 0b10000000:
+    n_byte -= 1
+else:
+    return False
 
-        byte = num & 0xFF
-
-        if n_byte == 0:
-            if (byte & 0b10000000) == 0b00000000:
-                n_byte = 0
-            elif (byte & 0b11100000) == 0b11000000:
-                n_byte = 1
-            elif (byte & 0b11110000) == 0b11100000:
-                n_byte = 2
-            elif (byte & 0b11111000) == 0b11110000:
-                n_byte = 3
-            else:
-                return False
-
-        elif n_byte > 0:
-            if (byte & 0b11000000) == 0b10000000:
-                n_byte -= 1
-            else:
-                return False
-
-    return n_byte == 0
+return n_byte == 0
