1ace6317cb19fc0ff1dfef4b2dd81543e3d9623d
Dr-Abder <9907@holbertonstudents.com>
2025-10-17 17:01:27 +0000

    0-slide_line

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -3,11 +3,9 @@
 
 #include <stddef.h>
 
-/* Définir les macros pour les directions */
-#define SLIDE_LEFT /* TON CODE : quelle valeur ? 0 ou 1 ? */
-#define SLIDE_RIGHT /* TON CODE : quelle valeur ? 0 ou 1 ? */
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
-/* Prototype de ta fonction */
 int slide_line(int *line, size_t size, int direction);
 
-#endif
+#endif /* SLIDE_LINE_H */
